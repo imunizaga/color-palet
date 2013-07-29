4bd0b8f5432d5bd1b2e9589a0a4e993c62467c12
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2013-07-29 14:08:25 -0400

    Now the color of the boxes can be changed by clicking on them

--- popup.js
+++ popup.js
@@ -11,21 +11,49 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var colorPalet = {
         drawColorBoxes: function () {
-            var inputs, i, input, boxes, box;
+            var inputs, i, input, boxes, buttons;
             inputs = document.getElementsByClassName('color-input');
             for (i = 0; i < inputs.length; i += 1) {
                 input = inputs[i];
                 input.value = "FFFFFF";
+                this.bindInputChange(input);
             }
 
             boxes = document.getElementsByClassName('color-box');
             for (i = 0; i < boxes.length; i += 1) {
-                box = boxes[i];
-                debugger;
+                this.bindBoxClick(boxes[i]);
             }
-        }
 
-        
+            buttons = document.getElementsByClassName('js-close-button');
+
+            for (i = 0; i < buttons.length; i += 1) {
+                this.bindCloseButtonClick(buttons[i]);
+            }
+        },
+
+        bindCloseButtonClick: function (button) {
+            button.onclick = function (e) {
+                e.stopPropagation();
+                var options = this.parentElement;
+                options.className = options.className + " hidden";
+            };
+        },
+
+        bindInputChange: function (input) {
+            input.onkeyup = function () {
+                var box = this.parentElement.parentElement;
+                box.style.backgroundColor = "#" + this.value;
+            };
+        },
+
+        bindBoxClick: function (box) {
+            box.onclick = function () {
+                var options = this.children[0];
+                if (options.className.indexOf('hidden') >= 0) {
+                    options.className = options.className.replace(/\bhidden\b/, '');
+                }
+            };
+        }
     };
     
     colorPalet.drawColorBoxes();
